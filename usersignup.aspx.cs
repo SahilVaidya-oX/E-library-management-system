a4030a45cc05d60e32cbb5eb70ce7492bc5c6afd
Sahil  Vaidya <84224976+SahilVaidya-oX@users.noreply.github.com>
2021-05-15 14:11:51 +0530

    Add files via upload

---
+++ usersignup.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ElibraryManagement
+{
+    public partial class usersignup : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Signup button click event
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (checkMemberExists())
+            {
+                Response.Write("<script>alert('Member ID exists');</script>");
+            }
+            else
+            {
+
+            }
+            signUpNewUser();
+        }
+
+        // User defined 
+        bool checkMemberExists()
+        {
+            //Response.Write("<script>alert('Testing');</script>");
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from member_master_tbl where member_id='" + TextBox8.Text + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt); //dt is table inside query whatever input is taken store data in datatable
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+                con.Close();
+                Response.Write("<script>alert('Sign up Successfull. Go to user login to login');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+
+        }
+
+        // User Defined method
+        void signUpNewUser()
+        {
+            //Response.Write("<script>alert('Testing');</script>");
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO member_master_tbl" +
+                    "(full_name,dob,contact_no,email,state,city,pincode,full_address,member_id,password,account_status) values" +
+                    "(@full_name,@dob,@contact_no,@email,@state,@city,@pincode,@full_address,@member_id,@password,@account_status)", con);
+
+                cmd.Parameters.AddWithValue("@full_name", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@dob", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@contact_no", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@state", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@city", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@pincode", TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@full_address", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@member_id", TextBox8.Text.Trim());
+                cmd.Parameters.AddWithValue("@password", TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@account_status", "pending");
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Sign up Successfull. Go to user login to login');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+    }
+}
